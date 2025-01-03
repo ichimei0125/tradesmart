198197828b1debd03538d7676d2568fdf4d2cef6
ichimei0125 <ichimei0125@gmail.com>
2025-01-03 18:09:51 +0900

    bugfix, add more convert

--- tradeengine/models/trade.py
+++ tradeengine/models/trade.py
@@ -65,10 +65,42 @@ def _sort_trades_desc_heapq(trades: List[Trade]) -> List[Trade]:
     
     return sorted_trades
 
-def trade_2_candlestick_minutes(trades:List[Trade], minute:int, cached:Optional[List[CandleStick]]=None) -> List[CandleStick]:
+def trade_2_candlestick_seconds(trades:List[Trade], second:int, cached:Optional[List[CandleStick]]=None) -> List[CandleStick]:
+    if type(second) is not int or not (second > 0 and second <=60):
+        raise ValueError(f'second must int and in 1~60, wrong second: {second}')
     trades = sort_trades_desc(trades)
+    lastest_open_time_second = (trades[0].execution_time.second // second) * second
+    open_time = trades[0].execution_time.replace(second=lastest_open_time_second, microsecond=0)
+
+    candlesticks = []
+    tmp_trades_price = []
+    tmp_volume:float = 0
+    for i in range(len(trades)):
+        if trades[i].execution_time < open_time:
+            new_candlestick = CandleStick(
+                open=tmp_trades_price[-1],
+                close=tmp_trades_price[0],
+                high=np.max(tmp_trades_price),
+                low=np.min(tmp_trades_price),
+                volume=tmp_volume,
+                opentime=open_time,
+            )
+            candlesticks.append(new_candlestick)
+            open_time -= timedelta(second=second)
+            tmp_trades_price = []
+
+        if cached and open_time == cached[0].opentime: # if open_time != cached[0].opentime means different time scale, won't use cached data
+            candlesticks += cached[1:]
+            break
+
+        tmp_trades_price = np.append(tmp_trades_price, trades[i].price)
+        tmp_volume += trades[i].size
+    return candlesticks
+
+def trade_2_candlestick_minutes(trades:List[Trade], minute:int, cached:Optional[List[CandleStick]]=None) -> List[CandleStick]:
     if type(minute) is not int or not (minute > 0 and minute <= 60):
         raise ValueError(f'minute must int and in 1~60, wrong minute: {minute}')
+    trades = sort_trades_desc(trades)
     lastest_open_time_minute = (trades[0].execution_time.minute // minute) * minute
     open_time = trades[0].execution_time.replace(minute=lastest_open_time_minute, second=0, microsecond=0)
 
@@ -97,9 +129,44 @@ def trade_2_candlestick_minutes(trades:List[Trade], minute:int, cached:Optional[
         tmp_volume += trades[i].size
     return candlesticks
 
-def trade_2_candlestick_days(trades:List[Trade], day:int, cached:Optional[List[CandleStick]]) -> List[CandleStick]:
+def trade_2_candlestick_hours(trades:List[Trade], hour:int, cached:Optional[List[CandleStick]]=None) -> List[CandleStick]:
+    if type(hour) is not int or not (hour > 0 and hour <= 24):
+        raise ValueError(f'hour must int and in 1~24, wrong hour: {hour}')
     trades = sort_trades_desc(trades)
-    open_time = trades[0].execution_time.replace(hour=0 ,minute=0, second=0, microsecond=0)
+    lastest_open_time_hour = (trades[0].execution_time.hour // hour) * hour
+    open_time = trades[0].execution_time.replace(hour=lastest_open_time_hour, minute=0, second=0, microsecond=0)
+
+    candlesticks = []
+    tmp_trades_price = []
+    tmp_volume:float = 0
+    for i in range(len(trades)):
+        if trades[i].execution_time < open_time:
+            new_candlestick = CandleStick(
+                open=tmp_trades_price[-1],
+                close=tmp_trades_price[0],
+                high=np.max(tmp_trades_price),
+                low=np.min(tmp_trades_price),
+                volume=tmp_volume,
+                opentime=open_time,
+            )
+            candlesticks.append(new_candlestick)
+            open_time -= timedelta(hour=hour)
+            tmp_trades_price = []
+
+        if cached and open_time == cached[0].opentime: # if open_time != cached[0].opentime means different time scale, won't use cached data
+            candlesticks += cached[1:]
+            break
+
+        tmp_trades_price = np.append(tmp_trades_price, trades[i].price)
+        tmp_volume += trades[i].size
+    return candlesticks
+
+def trade_2_candlestick_days(trades:List[Trade], day:int, cached:Optional[List[CandleStick]]) -> List[CandleStick]:
+    if type(day) is not int or not (day > 0 and day <= 28):
+        raise ValueError(f'day must int and in 1~28, wrong day: {day}')
+    trades = sort_trades_desc(trades)
+    lastest_open_time_day = (trades[0].execution_time.day // day) * day
+    open_time = trades[0].execution_time.replace(day=lastest_open_time_day, hour=0 ,minute=0, second=0, microsecond=0)
 
     candlesticks = []
     tmp_trades_price = []
