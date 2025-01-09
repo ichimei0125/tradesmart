0592cbd3a0a9cd0b81e534fd074f038a79088fe8
ichimei0125 <ichimei0125@gmail.com>
2025-01-09 23:37:19 +0900

    use argparse

--- __main__.py
+++ __main__.py
@@ -1,3 +1,5 @@
+import argparse
+import asyncio
 from api.crypto.exchange import Exchange
 from api.crypto.bitflyer.bitflyer import Bitflyer
 from config.config import Config
@@ -13,9 +15,32 @@ def simulator() -> None:
     c = Config()
     e = Bitflyer(c.bitflyer.symbols, c.bitflyer.dry_run_symbols)
     Simulator(e).run(last_days=10)
-    # Simulator(e).test_ml()
+
+async def update_model() -> None:
+    # TODO: sepreate
+    from bot.simulator import Simulator
+    c = Config()
+    e = Bitflyer(c.bitflyer.symbols, c.bitflyer.dry_run_symbols)
+    Simulator(e).test_ml()
     
 
 if __name__ == '__main__':
-    # main()
-    simulator()
+    parser = argparse.ArgumentParser(description="tradebot")
+    subparsers = parser.add_subparsers(dest="mode", required=True, help="select a mode to run")
+
+    subparsers.add_parser("trade", help="trade mode, default mode")
+    subparsers.add_parser("simulate", help="simulate past data")
+    subparsers.add_parser("update_model", help="update bot's model")
+
+    args = parser.parse_args()
+
+    # default
+    if args.mode is None:
+        args.mode = "trade"
+
+    if args.mode == "trade":
+        main()
+    elif args.mode == "simulate":
+        simulator(args.last_days)
+    elif args.mode == "update_model":
+        asyncio.run(update_model())
