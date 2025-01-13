1e79e338ac96380c3d25e031a6a9093a6426c96f
ichimei0125 <ichimei0125@gmail.com>
2025-01-13 21:30:33 +0900

    adjustable predict numbers

--- tradeengine/core/ml/lstm.py
+++ tradeengine/core/ml/lstm.py
@@ -13,20 +13,24 @@ from tradeengine.tools.common import create_folder_if_not_exists
 from tradeengine.tools.constants import ConstantPaths
 
 class MarketLSTM:
-    def __init__(self, candlesticks:List[CandleStick]):
+    def __init__(self, candlesticks:List[CandleStick], look_back:int=3, look_forward:int=1):
+        """look_back: number of candlestick to predict
+        look_forward: predict candlestick numbers
+        """
         self.prices = list(reversed(get_candlestick_prices(candlesticks)))
         self.times = list(reversed(get_candlestick_prices(candlesticks, 'opentime')))
 
-    def preprocess_data(self, look_back:int=3):
-        """look_back: past days"""
+        self.look_back = look_back
+        self.look_forward = look_forward
 
+    def preprocess_data(self):
         scaler = MinMaxScaler(feature_range=(0, 1))
         scaled_data = scaler.fit_transform(np.array(self.prices).reshape(-1, 1))
 
         X, y = [], []
-        for i in range(look_back, len(scaled_data)):
-            X.append(scaled_data[i - look_back:i, 0])
-            y.append(scaled_data[i, 0])
+        for i in range(self.look_back, len(scaled_data)-self.look_forward+1):
+            X.append(scaled_data[i - self.look_back:i, 0])
+            y.append(scaled_data[i:i+self.look_forward, 0])
 
         return np.array(X), np.array(y), scaler
 
@@ -35,7 +39,7 @@ class MarketLSTM:
             LSTM(50, return_sequences=True, input_shape=input_shape),
             LSTM(50, return_sequences=False),
             Dense(25),
-            Dense(1)
+            Dense(self.look_forward)
         ])
         model.compile(optimizer='adam', loss='mean_squared_error')
         return model
@@ -46,8 +50,9 @@ class MarketLSTM:
         split_idx = int(len(X) * train_test_ratio)
         X_train, y_train = X[:split_idx], y[:split_idx]
         X_test, y_test = X[split_idx:], y[split_idx:]
-        train_times = self.times[:split_idx]
-        test_times = self.times[split_idx+3:]
+        # train_times = self.times[:split_idx]
+        test_times = self.times[split_idx + self.look_back: split_idx + self.look_back + len(X_test)]
+
 
         X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
         X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
@@ -61,14 +66,19 @@ class MarketLSTM:
 
         # predict
         predictions = model.predict(X_test)
-        predictions = scaler.inverse_transform(predictions.reshape(-1, 1))
-
-        real_values = scaler.inverse_transform(y_test.reshape(-1, 1))
+        predictions = scaler.inverse_transform(predictions)
+        real_values = scaler.inverse_transform(y_test)
 
         if show_pic:
             plt.figure(figsize=(10, 6))
             plt.plot(self.times, self.prices, label='Real Prices', marker='o')
-            plt.plot(test_times, predictions, label='Predicted Prices', marker='x')
+            for i in range(self.look_forward):
+                plt.plot(
+                    test_times,
+                    predictions[:, i],
+                    label=f'Predicted Price +{i+1}',
+                    marker='x'
+                )
             plt.legend()
             plt.xlabel('Time')
             plt.ylabel('Price')
@@ -85,14 +95,19 @@ class MarketLSTM:
 
         # predict
         predictions = model.predict(X)
-        predictions = scaler.inverse_transform(predictions.reshape(-1, 1))
+        predictions = scaler.inverse_transform(predictions)
 
-        real_values = scaler.inverse_transform(y.reshape(-1, 1))
+        real_values = scaler.inverse_transform(y)
 
         if show_pic:
             plt.figure(figsize=(10, 6))
             plt.plot(real_values, label='Real Prices', marker='o')
-            plt.plot(predictions, label='Predicted Prices', marker='x')
+            for i in range(self.look_forward):
+                plt.plot(
+                    predictions[:, i],
+                    label=f'Predicted Price +{i+1}',
+                    marker='x'
+                )
             plt.legend()
             plt.xlabel('Time')
             plt.ylabel('Price')
@@ -109,11 +124,13 @@ def _get_model_path(name:str) -> Path:
     return folder.joinpath(file_name)
 
 def lstm_training(name, candlesticks: List[CandleStick]) -> None:
-    engine = MarketLSTM(candlesticks)
-    engine.train_model(save_path=_get_model_path(name), show_pic=False)
+    engine = MarketLSTM(candlesticks, look_back=20, look_forward=3)
+    engine.train_model(save_path=_get_model_path(name), show_pic=True)
 
 def lstm_run(name, candlesticks: List[CandleStick]) -> List[float]:
-    engine = MarketLSTM(candlesticks)
+    engine = MarketLSTM(candlesticks, look_back=20, look_forward=3)
     res = engine.predict(save_path=_get_model_path(name))
+
+    print(res)
 
     return res 
